eda9e521661754e226bddb128e7f2c2f7c1463ad
porlanagustin <porlan.agustin@gmail.com>
2023-07-17 20:36:40 -0300

    commit

---
+++ espacio-silvestre/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "../src/components/layout/navbar/Navbar";
+import Footer from "../src/components/layout/footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
